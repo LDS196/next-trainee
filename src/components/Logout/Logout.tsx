d443f39b16c0164e9b1cb89fa2ce693fccff2eda
LDS196 <lihachevd87@gmail.com>
2023-05-23 12:12:45 +0400

    fix Logout.tsx

--- src/components/Logout/Logout.tsx
+++ src/components/Logout/Logout.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Logout = () => {
     return (
         <div>
-            fhhdfhgfghgfhgfh
-            dddddddddddddddd
+          Logout
         </div>
     );
 };
